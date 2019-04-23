91f8c1eb898437afea570237885b8d0d4e2917eb
Isaac Stong <stongi@andrews.edu>
2019-04-23 16:43:23 -0400

    Added Pressure Puzzle and did work on maze puzzle

---
+++ Assets/Scripts/PressurePad.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePad : MonoBehaviour
+{
+    bool isSolved;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isSolved = true;
+        print("solved");
+    }
+}
